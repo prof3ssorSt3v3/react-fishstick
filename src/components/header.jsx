119df6b1ef586b6571736e9e4174e7a84e139797
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-03-15 20:33:08 -0400

    add header component and style

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/header.module.css';
+
+export default function Header(props) {
+  return (
+    <header className={styles.header}>
+      <h1>Default Heading</h1>
+    </header>
+  );
+}
